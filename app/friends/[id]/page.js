abd6bdf5284583251df77e70efa9a7c9116c058d
Oaes Kuruni <oaeskuruni27@gmail.com>
2026-05-12 20:27:40 +0600

    friends details page issues fixed

--- app/friends/[id]/page.js
+++ app/friends/[id]/page.js
@@ -1,6 +1,6 @@
 import { notFound } from "next/navigation";
-import FriendDetails from "../../components/friends/FriendsDetails.jsx"
-import {getFriendById} from "../../lib/friend.js"
+import FriendDetails from "../../../components/friends/FriendsDetails.jsx";
+import {getFriendById} from "../../../lib/friend.js";
 
 export default async function FriendDetailsPage({ params }) {
   const { id } = await params;
